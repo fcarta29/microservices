44fb1a09e83f5b8b12d5d23a221b81af8c2ff493
Frank Carta <fcarta@vmware.com>
2016-10-15 13:33:04 -0700

    Fixing journal service implementation to function more like service

---
+++ microservices-service/service-journal/src/main/java/com/vmware/pso/samples/services/journal/controller/TopicsController.java
@@ -0,0 +1,30 @@
+package com.vmware.pso.samples.services.journal.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/topics")
+public class TopicsController {
+
+    final static String TOPICS_KEY = "topics";
+
+    @Autowired
+    @Qualifier("redisTemplate")
+    private RedisTemplate<String, String> redisTemplate;
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    final public @ResponseBody List<String> get() {
+        final List<String> topics = new ArrayList<String>();
+        topics.addAll(redisTemplate.opsForZSet().range(TOPICS_KEY, 1, Long.MAX_VALUE));
+        return topics;
+    }
+}
--- microservices-service/service-journal/src/main/java/com/vmware/pso/samples/services/journal/reciever/JournalReceiver.java
+++ microservices-service/service-journal/src/main/java/com/vmware/pso/samples/services/journal/reciever/JournalReceiver.java
@@ -4,6 +4,10 @@ import java.util.concurrent.CountDownLatch;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.vmware.pso.samples.core.dto.TopicDto;
 
 public class JournalReceiver {
 
@@ -11,13 +15,23 @@ public class JournalReceiver {
 
     private final CountDownLatch latch;
 
+    final static String TOPICS_KEY = "topics";
+
+    final static String TOPIC_TEAM_DEFAULT = "2"; // for team 2 since this is the default for the testing
+
+    @Autowired
+    @Qualifier("redisTemplate")
+    private RedisTemplate<String, String> redisTemplate;
+
     @Autowired
     public JournalReceiver(final CountDownLatch latch) {
         this.latch = latch;
     }
 
-    public void receiveMessage(final String message) {
-        LOG.info("Received <" + message + ">");
+    public void receiveMessage(final TopicDto topicDto) {
+        LOG.info(String.format("Received reservation request for %s", topicDto.getMessage()));
+        redisTemplate.opsForList().rightPush(TOPIC_TEAM_DEFAULT, topicDto.getMessage());
+        redisTemplate.opsForZSet().incrementScore(TOPICS_KEY, TOPIC_TEAM_DEFAULT, 1);
         latch.countDown();
     }
 }
